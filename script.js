a4d63a00717fa2109ed70f2f814b7b35e640aa0a
Basant <bsntsngh16@gmail.com>
2023-04-16 14:37:09 +0530

    Added Comments

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
+// Initialize the seconds and milli-seconds to zero
 let dataMilli = 0;
 let dataSec = 0;
+// Put the value of seconds and milliseconds in there respective html tag
 document.getElementById('displayMilli').innerText = dataMilli;
 document.getElementById('displaySec').innerText = dataSec;
 
+//Get the start button element from html
 let startButton = document.getElementById('start');
+// This keyword is to check if there is a start interval running or not
 let interval;
+
+//Event Listner for Start Button
 startButton.addEventListener('click',function(){
     console.log("Inside Start Button");
     if(interval == undefined)
     {
         interval = setInterval(function(){
-            if(dataSec == 99)
+            // Check if limit of stop watch is reached or not
+            if(dataSec == 99 && dataMilli == 99)
             {
                 clearInterval(interval);
                 alert("StopWatch Limit Reached!!!");
             }
+            // Check when to reset millseconds and increase second counter
             else if(dataMilli == 99)
             {
                 dataSec += 1;
@@ -22,6 +30,7 @@ startButton.addEventListener('click',function(){
                 document.getElementById('displayMilli').innerText = dataMilli;
                 document.getElementById('displaySec').innerText = dataSec;
             }
+            // increase milli-seconds counter
             else
             {
                 dataMilli += 1;
@@ -32,19 +41,24 @@ startButton.addEventListener('click',function(){
     }
 });
 
+// Get the stop button from html
 let stopButton = document.getElementById('stop');
+// Event Listner for stop button
 stopButton.addEventListener('click',function(){
     console.log("Inside Stop Button");
+    //Check if interval already exists or not
     if(interval != undefined)
     {
         clearInterval(interval);
         interval = undefined;
     }
 });
-
+// Get the reset button from html
 let resetButton = document.getElementById('reset');
+// Event Listner for reset button
 resetButton.addEventListener('click',function(){
     console.log("Inside Reset Button");
+    //Check if interval already exists or not
     if(interval != undefined)
     {
         clearInterval(interval);
@@ -52,6 +66,7 @@ resetButton.addEventListener('click',function(){
     }
     dataMilli = 0;
     dataSec = 0;
+    //set the reset value in the respective html tag
     document.getElementById('displayMilli').innerText = dataMilli;
     document.getElementById('displaySec').innerText = dataSec;
 });
